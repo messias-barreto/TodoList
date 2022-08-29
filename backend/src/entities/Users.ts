41a6daec06ae7ba71dedd5bac349dc779f418c58
messiasbarreto <messias.barreto@unisuam.edu.br>
2022-08-29 02:15:43 -0300

    Adionando paginacao na tela de work

--- backend/src/entities/Users.ts
+++ backend/src/entities/Users.ts
@@ -1,6 +1,5 @@
 import { Column, Entity, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
-import { RefreshToken } from "./RefreshToken";
 
 @Entity()
 export class Users {
@@ -14,9 +13,6 @@ export class Users {
     email: string;
     @Column({type: 'text'})
     password: string;
-
-    @OneToMany(type => RefreshToken, refreshToken => refreshToken.user)
-    refreshTokens: RefreshToken
 
     constructor() {
         if(!this.id){
